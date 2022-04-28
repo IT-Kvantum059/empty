61978528df9dc6eb913941bb6f767d17d28fe00d
darkusha-ds <mts04@inbox.ru>
2022-04-28 22:24:47 +0500

    add page for DRF

--- web_django/urls.py
+++ web_django/urls.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
-from django.urls import path
 from . import views
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', views.index),
+    path('apis', include(router.urls)),
     path('input', views.input_commands),
-    path('get_cmd', views.output_commands)
+    path('get_cmd', views.output_commands),
+    path('api-auth/', include('rest_framework.urls')),
 ]
